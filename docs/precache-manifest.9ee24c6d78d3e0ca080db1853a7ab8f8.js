e2814e586c6afe58b5e2ec3b015f17104c2d3521
Razvan Cristea <razcristea@gmail.com>
2019-12-02 14:08:21 +0200

    build deploy

---
+++ docs/precache-manifest.9ee24c6d78d3e0ca080db1853a7ab8f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48a7cfec35ef146308da024da0e935b8",
+    "url": "/react-calc/index.html"
+  },
+  {
+    "revision": "0f15c5b9672684afa4a9",
+    "url": "/react-calc/static/css/main.d85e9332.chunk.css"
+  },
+  {
+    "revision": "d3954ec4ba5ae5c46f1d",
+    "url": "/react-calc/static/js/2.9c70cfda.chunk.js"
+  },
+  {
+    "revision": "0f15c5b9672684afa4a9",
+    "url": "/react-calc/static/js/main.9eb3f49b.chunk.js"
+  },
+  {
+    "revision": "a9f5279290cf2f8b0326",
+    "url": "/react-calc/static/js/runtime-main.1ba8fba5.js"
+  }
+]);
